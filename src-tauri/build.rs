54d85ac75e2fe943d229f1c9679c84ec851d260f
smellyb <59832246+smellyb@users.noreply.github.com>
2026-08-21 01:14:53 +0800

    1234

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,5 @@
 fn main() {
-    tauri_build::build()
+    if let Err(err) = tauri_build::try_build(tauri_build::Attributes::new()) {
+        eprintln!("Warning: tauri_build helper returned an error: {err}");
+    }
 }
